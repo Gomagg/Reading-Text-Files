d302caec606c3547d342a50c37fa1b9df937ebde
Adeyeye Emmanuel Oluwagbemi <101685525+Gomagg@users.noreply.github.com>
2022-06-01 03:32:54 -1200

    Python program that read text files
    This python program actually read any text file and count the number of each word in a text file.

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 def read_file_content(filename):
-
-	#Answer	
-  with open("./story.txt", "r") as openfile:
-
-		read_the_file = openfile.read()
-
-	return read_the_file
-
-	
-
-def countwords():
-
 	#Answer
-
-	text = read_file_content("./story.txt")
-
-	print(text)
-
-	split_text = text.split()
-
-	count = {}
-
-	for n in split_text:
-
-		if n in count:
-
-			count[n] += 1
-
-		else:
-
-			count[n] = 1
-
-	return count
-
+	with open("./story.txt", "r") as openfile:
+		read_the_file = openfile.read()
+	return read_the_file
 	
-
+def countwords():
+	#Answer
+	text = read_file_content("./story.txt")
+	print(text)
+	split_text = text.split()
+	count = {}
+	for n in split_text:
+		if n in count:
+			count[n] += 1
+		else:
+			count[n] = 1
+	return count
+	
 print(countwords())
